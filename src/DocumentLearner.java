974861a31cec5fa3212add9974202c734878d010
s1719920 <d.r.debie@student.utwente.nl>
2017-01-05 15:35:58 +0100

    made it possible to request a hashmap with all file names and a hashmap with word/number of occurrences of the word per file

--- src/DocumentLearner.java
+++ src/DocumentLearner.java
@@ -12,32 +12,38 @@ import java.util.StringTokenizer;
 public class DocumentLearner {
 
     String DirectoryPath = "D:\\school\\module 6 Intelligent Interaction Design\\AI\\Interactive Learner\\blogs";
-    HashMap<String, Integer> words = new HashMap<String, Integer>();
+    HashMap<String, HashMap<String, Integer>> files = new HashMap<String, HashMap<String, Integer>>();
 
     public HashMap createList() {
         File dir = new File(DirectoryPath);
         File[] dirList = dir.listFiles();
         if (dirList != null) {
             for (File folder : dirList) {
-                System.out.println(folder.getName());
-                for (File file : folder.listFiles()) {
-                    System.out.println(file.getName());
-                    readFile(file);
+                //System.out.println(folder.getName());
+                if (folder.listFiles() != null) {
+                    for (File file : folder.listFiles()) {
+                        //System.out.println(file.getName());
+                        if (!files.containsKey(file.getName())) {
+                            files.put(file.getName(), readFile(file));
+                        }
+                    }
                 }
+
             }
         }
-        Set<String> keys = words.keySet();
-        int totalValue = 0;
-        for (String key : keys) {
-            int value = words.get(key);
-            totalValue = totalValue + value;
-            System.out.println("woord: " + key + " aantal: " + value);
-        }
-        //System.out.println("woord aantal: " + keys.size());
-        System.out.println("aantal woorden in totaal: " + totalValue);
-        return words;
+//        Set<String> keys = words.keySet();
+//        int totalValue = 0;
+//        for (String key : keys) {
+//            int value = words.get(key);
+//            totalValue = totalValue + value;
+//            System.out.println("woord: " + key + " aantal: " + value);
+//        }
+//        //System.out.println("woord aantal: " + keys.size());
+//        System.out.println("aantal woorden in totaal: " + totalValue);
+        return files;
     }
-    public void readFile(File file) {
+    public HashMap readFile(File file) {
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             StringBuilder sb = new StringBuilder();
@@ -49,7 +55,7 @@ public class DocumentLearner {
             String notNormalized = sb.toString();
             notNormalized = notNormalized.toLowerCase();
             notNormalized = notNormalized.replaceAll("[^A-Za-z0-9 ]", "");
-            System.out.println(notNormalized);
+            //System.out.println(notNormalized);
             StringTokenizer tokenizer = new StringTokenizer(notNormalized);
             while (tokenizer.hasMoreElements()) {
                 String nextElement = (String) tokenizer.nextElement();
@@ -62,6 +68,19 @@ public class DocumentLearner {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        return words;
+    }
+
+    public void getFilesInfo() {
+        Set<String> keys = files.keySet();
+        for (String key : keys) {
+            HashMap<String, Integer> result = files.get(key);
+            Set<String> words = result.keySet();
+            for (String word : words) {
+                int value = result.get(word);
+                System.out.println("FileName: " + key + "       Woord: " + word + "      Aantal voorkomen: " + value);
+            }
         }
     }
 }
