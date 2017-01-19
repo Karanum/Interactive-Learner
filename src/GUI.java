12df21dcaa9852089af8a20a62caeed2021cb678
s1719920 <d.r.debie@student.utwente.nl>
2017-01-19 02:15:06 +0100

    made some more changes to the GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,46 +1,27 @@
-import javafx.stage.FileChooser;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.File;
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Created by Dylan on 18-1-2017.
  */
 public class GUI {
 
-    private JButton button1;
+    private JButton insertData;
     private JPanel panel1;
     private JButton startLearningButton;
     private JTabbedPane tabbedPane1;
-    private JLabel label1;
+    private JLabel warningLabel;
     private JLabel pathMessage;
     private static String directoryPath;
 
 
     public GUI() {
-//        try {
-//            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-//                System.out.println(info.getName());
-//                if("Metal".equals(info.getName())) {
-//                    UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (UnsupportedLookAndFeelException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (InstantiationException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
+
         JFrame frame = new JFrame("Interactive Learner");
-        label1.setText("<html>Important! The folder selected must contain the train folders seperated. <br> " +
-                "Example: Train/M and Train/F where Train is the selected folder<html>");
-        pathMessage.setText("no train file specified");
+        warningLabel.setText("<html>Important! The folder selected must contain the train folders seperated. <br> " +
+                "Example: Train/M and Train/F where Train is the selected folder.<html>");
+        pathMessage.setText("No train folder specified. Please select a folder first ");
         frame.setContentPane(panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
@@ -49,7 +30,7 @@ public class GUI {
         frame.setVisible(true);
 
 
-        button1.addActionListener(new ActionListener() {
+        insertData.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileChooser = new JFileChooser();
@@ -58,7 +39,8 @@ public class GUI {
                 int returnValue = fileChooser.showOpenDialog(null);
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
                     directoryPath = fileChooser.getSelectedFile().getAbsolutePath();
-                    pathMessage.setText("selected folder: " + fileChooser.getSelectedFile().getName());
+                    //pathMessage.setText("selected folder: " + fileChooser.getSelectedFile().getName());
+                    pathMessage.setText("selected folder: " + fileChooser.getSelectedFile().getAbsolutePath());
 
                     //System.out.println(selectedFile);
                 }
